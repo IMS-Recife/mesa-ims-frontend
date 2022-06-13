0435257083defb16005cff553e14c3884690c752
filipeabessa <filipegbessa@gmail.com>
2022-06-13 11:20:59 -0300

    feat(projects): :sparkles: Add a request to edit project when the edit project form is sent

--- src/pages/interventions/index.vue
+++ src/pages/interventions/index.vue
@@ -31,16 +31,19 @@ onMounted(() => {
   isReady.value = true;
 
   apiGetListProjects().then((data) => {
-    console.log("projectsData", data);
     projects.value = data.data;
 
-    const formatedProject = projects.value.map((project) => {
+    const formatedProjects = projects.value.map((project) => {
       return {
         ...project,
         lastUpdate: moment(project.lastUpdate).format("DD/MM/YYYY"),
+        completedPercentage: project.completedPercentage
+          ? `${project.completedPercentage} %`
+          : "-",
       };
     });
-    table.rows = formatedProject;
+    table.totalRecordCount = formatedProjects.length;
+    table.rows = formatedProjects;
   });
 });
 
@@ -109,7 +112,7 @@ const table = reactive({
     },
   ],
   rows: projects.value,
-  totalRecordCount: 20,
+  totalRecordCount: 0,
   sortable: {
     order: "name",
     sort: "desc",
